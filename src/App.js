7cc77b4d5aadf91b8760bb0beece97c732cde980
DESKTOP-9LH7U24\alfakapa <aggeloskos@gmail.com>
2024-11-27 17:41:11 +0200

    Second

--- src/App.js
+++ src/App.js
@@ -99,7 +99,7 @@ export default function Home() {
       </header>
 
       {/* Hero Section */}
-      <section className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: 'url("/images/background.jpg")' }}>
+      <section className="relative w-full h-screen bg-cover bg-center" style={{ backgroundImage: `url(${process.env.PUBLIC_URL}/images/background.jpg)` }}>
         <div className="absolute inset-0 bg-black opacity-50"></div>
         <div className="relative z-10 flex items-center justify-center h-full text-center px-4">
           <div>
