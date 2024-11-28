2f36f638cfd1abda47c498f347882d4537e27d09
DESKTOP-9LH7U24\alfakapa <aggeloskos@gmail.com>
2024-11-29 00:11:50 +0200

    Active on mobile

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -22,22 +22,23 @@ export default function Portfolio({ projects, openModal }) {
             {projects.map((project) => (
               <div
                 key={project.id}
-                className="relative bg-gray-100 overflow-hidden shadow-xl transform scale-150 hover:scale-110 transition duration-300 cursor-pointer group"
+                className="relative bg-gray-100 overflow-hidden shadow-xl transform scale-150 group hover:scale-110 active:scale-110 transition duration-300 cursor-pointer group"
                 onClick={() => openModal(project)}
               >
-                {/* Image with grayscale filter and hover effect */}
+                {/* Image with grayscale filter and hover/active effect */}
                 <img
                   src={`${process.env.PUBLIC_URL}${project.images[0]}`}
                   alt={project.title}
                   className="w-full h-64 object-cover transition duration-300 filter grayscale group-hover:grayscale-0"
                 />
-
+              
                 {/* Overlay for title and description */}
-                <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300">
+                <div className="absolute inset-0 flex flex-col justify-center items-center bg-black bg-opacity-50 text-white opacity-0 group-hover:opacity-100 transition-opacity duration-300 group-active:opacity-100">
                   <h3 className="text-2xl font-semibold mb-4">{project.title}</h3>
                   <p className="px-4">{project.description}</p>
                 </div>
               </div>
+            
             ))}
           </div>
         </div>
